28451995bfa6da56584711c211ed3d45ae6b76f9
Keval <keval.shah36.ks@gmail.com>
2025-07-31 18:49:39 +0530

    Added mappings for Get,Post,Put,Patch and Delete

--- 02-Rest/src/main/java/__RestAPI/__Rest/dao/EmployeeDAO.java
+++ 02-Rest/src/main/java/__RestAPI/__Rest/dao/EmployeeDAO.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+
+    Employee findById(Integer id);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(Integer id);
 }
--- 02-Rest/src/main/java/__RestAPI/__Rest/service/EmployeeService.java
+++ 02-Rest/src/main/java/__RestAPI/__Rest/service/EmployeeService.java
@@ -7,4 +7,12 @@ import java.util.List;
 
 public interface EmployeeService {
     List<Employee> findAll();
+
+    Employee findById(Integer id);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(Integer id);
+
+
 }
