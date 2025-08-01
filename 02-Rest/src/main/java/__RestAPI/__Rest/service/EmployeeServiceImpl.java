40694dd1d51c56fa01c8c645cb66eee85568b7cd
Keval Home <kevalshah36.ks@gmail.com>
2025-08-01 07:56:30 +0530

    Changes in the DAO and DAO Implementation.

--- 02-Rest/src/main/java/__RestAPI/__Rest/service/EmployeeServiceImpl.java
+++ 02-Rest/src/main/java/__RestAPI/__Rest/service/EmployeeServiceImpl.java
@@ -1,42 +1,48 @@
 package __RestAPI.__Rest.service;
 
-import __RestAPI.__Rest.dao.EmployeeDAO;
+import __RestAPI.__Rest.dao.EmployeeRepository;
 import __RestAPI.__Rest.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(Integer id) {
-        return employeeDAO.findById(id);
+        Optional<Employee> employee = employeeRepository.findById(id);
+        Employee emp = null;
+        if(employee.isPresent()){
+            emp=employee.get();
+        }else{
+            throw new RuntimeException("Not found for Id : "+id);
+        }
+        return emp;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
-    @Transactional
     @Override
     public void deleteById(Integer id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 
 }
