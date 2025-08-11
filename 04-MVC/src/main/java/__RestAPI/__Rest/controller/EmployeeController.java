f1dad89cb34bdedc6e6aff9acb8224c8a8459c06
Keval Home <kevalshah36.ks@gmail.com>
2025-08-11 09:10:08 +0530

    Added CRUD operations for MVC

--- 04-MVC/src/main/java/__RestAPI/__Rest/controller/EmployeeController.java
+++ 04-MVC/src/main/java/__RestAPI/__Rest/controller/EmployeeController.java
@@ -5,8 +5,7 @@ import __RestAPI.__Rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,36 @@ public class EmployeeController {
     public String listEmployees(Model model){
         List<Employee> theEmployees = employeeService.findAll();
         model.addAttribute("employees",theEmployees);
-        return "list-employees";
+        return "employee/list-employees";
     }
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+        //create model attribuite for binding the form data
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee",theEmployee);
+        return "employee/employees-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId")int id,Model theModel){
+        Employee employee = employeeService.findById(id);
+        theModel.addAttribute("employee",employee);
+        return "employee/employees-form";
+    }
+
+
+    @PostMapping("/save")
+    public String addEmployee(@ModelAttribute Employee employee){
+        //save the employee
+
+        employeeService.save(employee);
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId")int Id){
+        employeeService.deleteById(Id);
+        return "redirect:/employees/list";
+    }
+
 }
--- 04-MVC/src/main/java/__RestAPI/__Rest/dao/EmployeeRepository.java
+++ 04-MVC/src/main/java/__RestAPI/__Rest/dao/EmployeeRepository.java
@@ -3,5 +3,8 @@ package __RestAPI.__Rest.dao;
 import __RestAPI.__Rest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
