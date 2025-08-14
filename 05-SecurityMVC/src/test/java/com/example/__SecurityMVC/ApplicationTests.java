531a8c331aa6fedcb74afa91d6ba0c2e8d22a363
Keval Home <kevalshah36.ks@gmail.com>
2025-08-14 07:29:34 +0530

    Creating the template for SpringSecurity in MVC

---
+++ 05-SecurityMVC/src/test/java/com/example/__SecurityMVC/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__SecurityMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
